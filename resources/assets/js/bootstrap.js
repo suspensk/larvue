a711d3ba841083d23ccd7fd352d13cb03dbea55f
Sofya <uspenskayasofyasergeevna@gmail.com>
2018-12-12 09:47:57 +0200

    bearer to axios default

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -37,7 +37,7 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
-
+window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('jwt');
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
