83c0d314b63b11500a454f60c6b9432b6db248d2
Sofya <uspenskayasofyasergeevna@gmail.com>
2018-12-12 09:47:36 +0200

    notes page under auth middleware,

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,12 @@ Route::post('logout', 'UserController@logout');
 Route::post('register', 'UserController@register');
 //Route::post('notes/add', 'NoteController@store');
 //Route::get('notes', 'NoteController@index');
-Route::resource('/notes', 'NoteController');
+
 Route::resource('/tags', 'TagController');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/category/{category}/tasks', 'CategoryController@tasks');
+    Route::resource('/notes', 'NoteController');
     Route::resource('/category', 'CategoryController');
     Route::resource('/task', 'TaskController');
 });
