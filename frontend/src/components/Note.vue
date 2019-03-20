12175bd91bb012a1678a0f4b9886dab868454f9e
Sofya <uspenskayasofyasergeevna@gmail.com>
2019-03-20 07:22:30 +0200

    onlyauthor edit

--- frontend/src/components/Note.vue
+++ frontend/src/components/Note.vue
@@ -49,20 +49,20 @@
             <v-btn icon slot="activator"> <v-icon>more_vert</v-icon> </v-btn>
 
             <v-list class="pa-0">
-                <v-list-tile v-if="$store.getters.isAuthenticated" ripple="ripple" rel="noopener" @click.stop="$emit('show-modal');">
+                <v-list-tile v-if="$store.getters.isAuthenticated && note.user.email == $store.getters.email" ripple="ripple" rel="noopener" @click.stop="$emit('show-modal');">
                     <v-list-tile-action> <v-icon>remove_circle_outline </v-icon> </v-list-tile-action>
                     <v-list-tile-content>
                         <v-list-tile-title>Delete</v-list-tile-title>
                     </v-list-tile-content>
                 </v-list-tile>
 
-                <v-list-tile v-if="$store.getters.isAuthenticated" ripple="ripple" rel="noopener" @click.stop="mode='edit'; editNote();">
+                <v-list-tile v-if="$store.getters.isAuthenticated && note.user.email == $store.getters.email" ripple="ripple" rel="noopener" @click.stop="mode='edit'; editNote();">
                     <v-list-tile-action> <v-icon>edit</v-icon> </v-list-tile-action>
                     <v-list-tile-content>
                         <v-list-tile-title>Edit</v-list-tile-title>
                     </v-list-tile-content>
                 </v-list-tile>
-                <v-list-tile ripple="ripple" rel="noopener">
+                <v-list-tile v-if="$store.getters.isAuthenticated" ripple="ripple" rel="noopener">
                     <v-list-tile-action>
                         <v-icon>fullscreen_exit</v-icon>
                     </v-list-tile-action>
